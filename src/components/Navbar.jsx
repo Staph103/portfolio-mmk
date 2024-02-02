5baeb2b6f21a5f4d4fbe581866e55becfb66e427
MMK <staph103@gmail.com>
2024-02-02 18:33:23 -0500

    worked on components and pages

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,92 @@
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Link, NavLink, useLocation } from 'react-router-dom'
+
+
+const navigation = [
+  { name: 'About', to: '/' , current: false},
+  { name: 'Resume', to: '/resume',current:false},
+  { name: 'Portfolio', to: '/portfolio',current:false},
+  { name: 'Contact', to: '/contact', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Navigation() {
+const location = useLocation()
+  return (
+    <Disclosure as="nav" className="bg-gray-800 py-6">
+      {({ open }) => (
+        <>
+          <div className="mx-auto px-4 sm:px-4 lg:px-4">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="h-7 w-7"
+                    src={''}
+                    alt="Your Company"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <NavLink
+                        key={item.name}
+                        to={item.to}
+                        className={({ isActive }) => (isActive ? "text-white text-sm px-3" :  'text-gray-300 hover:bg-gray-700 hover:text-white text-sm px-3')}
+                        // className={classNames(
+                        //   item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        //   'rounded-md px-3 py-2 text-sm font-medium'
+                        // )}
+                        aria-current={location.pathname ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </NavLink>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden absolute z-40 bg-gray-800 w-full">
+            <div className="space-y-1 px-2 pb-3 pt-2 ">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as={Link}
+                  to={item.to}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import PDF from "../assets/Mustapha_Kromah2.pdf";
+import { pdfjs, Document, Page } from "react-pdf";
+
+export default function Resume() {
+  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    "pdfjs-dist/build/pdf.worker.min.js",
+    import.meta.url
+  ).toString();
+
+  const maxWidth = 800;
+  const [numPages, setNumPages] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const [showPDF, setShowPDF] = useState(true);
+  const [containerWidth, setContainerWidth] = useState(window.innerWidth);
+
+  const handleDownload = () => {
+    fetch(PDF).then((response) => {
+      response.blob().then((blob) => {
+        const fileURL = URL.createObjectURL(blob);
+         open(fileURL, '_blank')  //alternative to the a link opens embedded adobe viewer !CHECK SAFARI!
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "Mustapha_Kromah2.pdf";
+        alink.click(); 
+      });
+    });
+  };
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  const handleClick = () => {
+    setShowPDF(!showPDF);
+  };
+
+  return (
+    <>
+      <div className="pb-2 mx-5">
+        <h3 className="pb-2 text-3xl">Resume</h3>
+        <h6>
+          Toggle here to view my{" "}
+          <a href="#image" onClick={handleClick} className="underline">
+            resume
+          </a>{" "}
+          as an image. 
+        </h6>
+        Click here to download my{" "}
+        <a href="#download" onClick={handleDownload} className="underline">
+          resume as a pdf.
+        </a>
+      </div>
+      {showPDF ? (
+        <div className="mx-5 flex flex-row">
+          <ul className="mx-5">
+            <p className="font-bold"> Front-end Proficiencies</p>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>Responsive Design</li>
+            <li>ReactJS</li>
+            <li>Bootstrap</li>
+            <li>MUI</li>
+            <li>TailwindCSS</li>
+          </ul>
+
+          <ul className="mx-5 basis-50">
+            <p className="font-bold"> Back-end Proficiencies</p>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL & Sequelize</li>
+            <li>MongoDB & Mongoose</li>
+            <li>REST APIs</li>
+            <li>GraphQL</li>
+          </ul>
+          <p>Also: being an awesome kitty. </p>
+        </div>
+      ) : (
+        <div className="">
+          <Document file={PDF} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page
+              pageNumber={pageNumber}
+              width={
+                containerWidth
+                  ? Math.min(containerWidth, maxWidth) - 30
+                  : maxWidth
+              }
+              renderAnnotationLayer={false}
+              renderTextLayer={false}
+            />
+          </Document>
+          <p>
+            Page {pageNumber} of {numPages}
+          </p>
+        </div>
+      )}
+    </>
+  );
+}
